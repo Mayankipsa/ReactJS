04420c3598fec11f61574ee40523bbcacd1c0ff8
Mayank <mayankipsa@gmail.com>
2021-02-20 10:21:45 +0530

    removed incomplete code

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import  Person from './Person/Person';
 
 class App extends Component {
-  state 
+  // state 
   render() {
     return (
       <div className="App">
         <h1>Hi, I am a React App</h1>
         <p>This is really working!</p>
-        <button onClick={}></button>
+        {/* <button onClick={}></button> */}
         <Person name="mayank" age="31">My Hobbies: Adventure Sports</Person>
         <Person name="shikha" age="30"/>
         <Person name="lata" age="58"/>

67476954255132f413fed7bcd945195da2fcc94e
Mayank <mayankipsa@gmail.com>
2021-02-20 12:07:21 +0530

    State usage in Component Example

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import './App.css';
 import  Person from './Person/Person';
 
 class App extends Component {
-  // state 
+  state ={
+    persons:[
+      {name:'Mayank',age:31},
+      {name:'Shikha',age:30},
+      {name:'Vinod',age:59},
+      {name:'Lata',age:58}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi, I am a React App</h1>
         <p>This is really working!</p>
         {/* <button onClick={}></button> */}
-        <Person name="mayank" age="31">My Hobbies: Adventure Sports</Person>
-        <Person name="shikha" age="30"/>
-        <Person name="lata" age="58"/>
-        <Person name="vinod" age="59"/>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies: Adventure Sports</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <Person name={this.state.persons[3].name} age={this.state.persons[3].age}/>
       </div>
       
     );

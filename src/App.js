d8cf1b1313c5ac7c51d391e789ff4df00104d364
Mayank <mayankipsa@gmail.com>
2021-02-21 20:50:19 +0530

    Dyanamic Styles to React Components

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
       { id: 3, name: 'Stephanie', age: 26 }
     ],
     str: "",
-    strLength:0,
+    strLength: 0,
     otherState: 'some other value',
     userName: "Mayank",
     showPersons: false
@@ -49,7 +49,8 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -71,12 +72,19 @@ class App extends Component {
           })}
         </div>
       );
+      style.backgroundColor = 'red';
     }
-
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
+    }
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button
           style={style}
           onClick={() => this.togglePersonsHandler()}>Toggle Persons</button>

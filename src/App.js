06fe1a00f1b142325ed29398a2c1edb1f5c89d22
Mayank <mayankipsa@gmail.com>
2021-02-21 21:37:54 +0530

    Radium: Removed radium references and usages.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium, { StyleRoot } from "radium";
 import Person from './Person/Person';
 import UserInput from "./UserInput/UserInput";
 import UserOutput from "./UserOutput/UserOutput";
@@ -90,7 +89,6 @@ class App extends Component {
       classes.push('bold');
     }
     return (
-      <StyleRoot>
         <div className="App">
           <h1>Hi, I'm a React App</h1>
           <p className={classes.join(' ')}>This is really working!</p>
@@ -99,11 +97,9 @@ class App extends Component {
             onClick={() => this.togglePersonsHandler()}>Toggle Persons</button>
           {persons}
         </div>
-      </StyleRoot>
-
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default Radium(App);
+export default App;

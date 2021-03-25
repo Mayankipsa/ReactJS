22a3bd72b89154259f222699b66a572a1982b1b7
Mayank <mayankipsa@gmail.com>
2021-03-25 21:16:48 +0530

    104. Using React.Fragment

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -11,11 +11,11 @@ class Person extends Component {
         }
         return (
             // <div className="Person" style={style}>
-            <Auxillary>
+            <React.Fragment>
                 <p key="i1" onClick={this.props.click}>I am {this.props.name} and I am {this.props.age} years old.</p>
                 <p key="i2">{this.props.children}</p>
                 <input key="i3" type="text" onChange={this.props.changed} value={this.props.name}></input>
-            </Auxillary>
+            </React.Fragment>
 
             // </div>
         )
